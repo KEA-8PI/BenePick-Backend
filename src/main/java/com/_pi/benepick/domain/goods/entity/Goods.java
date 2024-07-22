30ed1d78ad43b4ad5fb004e465a1ef6d37d373a5
Somi Nam <somi4219@gmail.com>
2024-07-22 17:40:46 +0900

    fix : 상품 관련 erd 수정

--- src/main/java/com/_pi/benepick/domain/goods/entity/Goods.java
+++ src/main/java/com/_pi/benepick/domain/goods/entity/Goods.java
@@ -19,13 +19,12 @@ public class Goods extends BaseJPATimeEntity {
     private Long id; //상품_id
     private String name; //이름
     private Long amounts; //수량
+    private String image; //상품 사진
     private String description; //설명
-    private String category; //카테고리
     private String goodsStatus; //상품응모상태
     private String seeds; //시드값
     private Long price; //정가
     private Long discountPrice; //할인가
-    private String goodsImage; //상품 사진
     private LocalDateTime raffleStartAt; //응모 시작일
     private LocalDateTime raffleEndAt; //응모 종료일
 }
