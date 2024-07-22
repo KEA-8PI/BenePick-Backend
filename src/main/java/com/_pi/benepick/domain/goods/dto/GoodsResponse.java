0eea7fd54cfcfd203182f1559d48c93619c37792
Somi Nam <somi4219@gmail.com>
2024-07-23 01:35:48 +0900

    fix : 상품 관련 erd 수정 적용

--- src/main/java/com/_pi/benepick/domain/goods/dto/GoodsResponse.java
+++ src/main/java/com/_pi/benepick/domain/goods/dto/GoodsResponse.java
@@ -18,14 +18,14 @@ public class GoodsResponse {
         private Long id; //상품_id
         private String name; //이름
         private Long amounts; //수량
+        private String image; //상품 사진
         private String description; //설명
-        private String category; //카테고리
         private String goodsStatus; //상품응모상태
         private Long price; //정가
         private Long discountPrice; //할인가
-        private String goodsImage; //상품 사진
         private LocalDateTime raffleStartAt; //응모 시작일
         private LocalDateTime raffleEndAt; //응모 종료일
+        private String category; //카테고리
     }
 
     // 상품 목록 조회
--- src/main/java/com/_pi/benepick/domain/goods/service/GoodsQueryServiceImpl.java
+++ src/main/java/com/_pi/benepick/domain/goods/service/GoodsQueryServiceImpl.java
@@ -1,10 +1,12 @@
 package com._pi.benepick.domain.goods.service;
 
 import com._pi.benepick.domain.goods.dto.GoodsResponse;
+import com._pi.benepick.domain.goods.entity.Categories;
 import com._pi.benepick.domain.goods.entity.Goods;
 import com._pi.benepick.domain.goods.repository.GoodsRepository;
 import com._pi.benepick.global.common.exception.ApiException;
 import com._pi.benepick.global.common.response.code.status.ErrorStatus;
+import jdk.jfr.Category;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -25,16 +27,16 @@ public class GoodsQueryServiceImpl implements GoodsQueryService{
     @Override
     public GoodsResponse.GoodsResponseDTO getGoodsInfo(Long goodsId) {
         Goods goods = goodsRepository.findById(goodsId).orElseThrow(() -> new ApiException(ErrorStatus._GOODS_NOT_FOUND));
+        Categories category =
         return GoodsResponse.GoodsResponseDTO.builder()
                 .id(goods.getId())
                 .name(goods.getName())
                 .amounts(goods.getAmounts())
+                .image(goods.getImage())
                 .description(goods.getDescription())
-                .category(goods.getCategory())
                 .goodsStatus(goods.getGoodsStatus())
                 .price(goods.getPrice())
                 .discountPrice(goods.getDiscountPrice())
-                .goodsImage(goods.getGoodsImage())
                 .raffleStartAt(goods.getRaffleStartAt())
                 .raffleEndAt(goods.getRaffleEndAt())
                 .build();
