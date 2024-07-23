88fae00776d66b65edb8cff553d53ac8428d5a11
wooing1084 <kmjung1515@naver.com>
2024-07-24 00:39:04 +0900

    feat: 위시리스트 연관관계 구현

--- src/main/java/com/_pi/benepick/domain/wishlists/controller/WishlistsController.java
+++ src/main/java/com/_pi/benepick/domain/wishlists/controller/WishlistsController.java
@@ -1,4 +1,36 @@
 package com._pi.benepick.domain.wishlists.controller;
 
+import com._pi.benepick.domain.goods.entity.Goods;
+import com._pi.benepick.domain.goods.repository.GoodsRepository;
+import com._pi.benepick.domain.members.entity.Members;
+import com._pi.benepick.domain.members.repository.MembersRepository;
+import com._pi.benepick.domain.wishlists.entity.Wishlists;
+import com._pi.benepick.domain.wishlists.repository.WishlistsRepository;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@Tag(name = "Wishlists", description = "위시리스트 API")
+@RequestMapping("/wishlists")
 public class WishlistsController {
+    private final WishlistsRepository wishlistsRepository;
+    private final MembersRepository membersRepository;
+    private final GoodsRepository goodsRepository;
+
+    @PostMapping("/add")
+    public void addWishlists() {
+        Goods goods = goodsRepository.findById(2L).get();
+        Members member = membersRepository.findById("02c1a091-0707-42d6-a822-018d8ea7e702").get();
+
+        Wishlists wishlists = Wishlists.builder()
+                .goodsId(goods)
+                .memberId(member)
+                .build();
+
+        wishlistsRepository.save(wishlists);
+    }
 }
--- src/main/java/com/_pi/benepick/domain/wishlists/entity/Wishlists.java
+++ src/main/java/com/_pi/benepick/domain/wishlists/entity/Wishlists.java
@@ -1,10 +1,15 @@
 package com._pi.benepick.domain.wishlists.entity;
 
 import com._pi.benepick.config.BaseJPATimeEntity;
+import com._pi.benepick.domain.goods.entity.Goods;
+import com._pi.benepick.domain.members.entity.Members;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -19,6 +24,10 @@ public class Wishlists extends BaseJPATimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id; //위시리스트_id
-    private String memberId; //멤버_id
-    private Long goodsId; //상품_id
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Members memberId; //멤버_id
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "goods_id")
+    private Goods goodsId; //상품_id
 }
