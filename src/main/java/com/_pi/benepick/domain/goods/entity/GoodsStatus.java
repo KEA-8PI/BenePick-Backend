2ee814683a1b8dcabf2bc986f8dd10e837b04c2c
Somi Nam <somi4219@gmail.com>
2024-07-23 15:50:18 +0900

    Api 47 feat/crud goods (#9)
    * fix : goods entity 수정
    
    * feat : 조회 api 추가
    
    * fix : goods entity 수정
    
    * feat : 조회 api 추가
    
    * fix : 상품 관련 erd 수정 적용
    
    * feat : CRUD api 추가
    
    * fix : soft delete 적용

---
+++ src/main/java/com/_pi/benepick/domain/goods/entity/GoodsStatus.java
@@ -0,0 +1,7 @@
+package com._pi.benepick.domain.goods.entity;
+
+public enum GoodsStatus {
+    PROGRESS, //진행
+    SCHEDULED, //예정
+    COMPLETED //종료
+}
--- src/main/java/com/_pi/benepick/domain/goods/repository/GoodsCategoriesRepository.java
+++ src/main/java/com/_pi/benepick/domain/goods/repository/GoodsCategoriesRepository.java
@@ -1,8 +1,14 @@
 package com._pi.benepick.domain.goods.repository;
 
-import com._pi.benepick.domain.goods.entity.Goods;
 import com._pi.benepick.domain.goods.entity.GoodsCategories;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface GoodsCategoriesRepository extends JpaRepository<GoodsCategories, Long> {
+    Optional<GoodsCategories> findByGoodsId(Long goodsId);
 }
