7a9212f6be7dace46b0aacc366fe98e73216db6b
Somi Nam <somi4219@gmail.com>
2024-07-29 17:32:03 +0900

    feat : 상품 수정 및 추가 reponse dto 분리

--- src/main/java/com/_pi/benepick/domain/goods/dto/GoodsResponse.java
+++ src/main/java/com/_pi/benepick/domain/goods/dto/GoodsResponse.java
@@ -133,6 +133,41 @@ public class GoodsResponse {
         private List<GoodsSearchResponseDTO> goodsSearchDTOList;
     }
 
+    // 상품 추가 및 수정
+    @Builder
+    @Getter
+    @AllArgsConstructor(access = AccessLevel.PRIVATE)
+    @NoArgsConstructor
+    public static class GoodsAddResponseDTO{
+        private Long id; //상품_id
+        private String name; //이름
+        private Long amounts; //수량
+        private String image; //상품 사진
+        private String description; //설명
+        private String goodsStatus; //상품응모상태
+        private Long price; //정가
+        private Long discountPrice; //할인가
+        private LocalDateTime raffleStartAt; //응모 시작일
+        private LocalDateTime raffleEndAt; //응모 종료일
+        private String category; //카테고리
+
+        public static GoodsAddResponseDTO of(Goods goods, String category){
+            return GoodsAddResponseDTO.builder()
+                    .id(goods.getId())
+                    .name(goods.getName())
+                    .amounts(goods.getAmounts())
+                    .image(goods.getImage())
+                    .description(goods.getDescription())
+                    .goodsStatus(goods.getGoodsStatus().name())
+                    .price(goods.getPrice())
+                    .discountPrice(goods.getDiscountPrice())
+                    .raffleStartAt(goods.getRaffleStartAt())
+                    .raffleEndAt(goods.getRaffleEndAt())
+                    .category(category)
+                    .build();
+        }
+    }
+
     // 상품 삭제
     @Builder
     @Getter
