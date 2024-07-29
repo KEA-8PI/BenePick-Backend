da005b2efa4d5722d92e5c175c30704392c7f463
2sojeong <dlgur200123@naver.com>
2024-07-29 19:15:23 +0900

    feat:사원 등록

--- src/main/java/com/_pi/benepick/domain/members/service/MembersCommandServiceImpl.java
+++ src/main/java/com/_pi/benepick/domain/members/service/MembersCommandServiceImpl.java
@@ -1,4 +1,15 @@
 package com._pi.benepick.domain.members.service;
 
+import com._pi.benepick.domain.members.repository.MembersRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
 public class MembersCommandServiceImpl implements MembersCommandService{
+    private final MembersRepository membersRepository;
+
+
 }
