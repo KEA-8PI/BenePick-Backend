e0e36cd8557f5daf2e45146491cb742b78e4ea1a
HyunseoPark <sspp8585@icloud.com>
2024-07-29 19:33:46 +0900

    Feat: mock-up code for draws & raffles

--- src/main/java/com/_pi/benepick/domain/raffles/controller/RafflesController.java
+++ src/main/java/com/_pi/benepick/domain/raffles/controller/RafflesController.java
@@ -1,18 +1,23 @@
 package com._pi.benepick.domain.raffles.controller;
 
+import com._pi.benepick.domain.goods.entity.Goods;
+import com._pi.benepick.domain.members.entity.Members;
 import com._pi.benepick.domain.raffles.dto.RafflesResponse;
+import com._pi.benepick.domain.raffles.entity.Raffles;
 import com._pi.benepick.domain.raffles.service.RafflesQueryService;
-import com._pi.benepick.domain.raffles.service.RafflesQueryServiceImpl;
 import com._pi.benepick.global.common.response.ApiResponse;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import java.time.LocalDateTime;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,6 +27,25 @@ import java.util.List;
 public class RafflesController {
 
     private final RafflesQueryService rafflesQueryService;
+
+    @Operation(summary = "응모하기 - Mockup API", description = "물품 아이디, 포인트를 사용하여 응모합니다.")
+    @PostMapping("/apply/{goodsId}")
+    public ApiResponse<Result> getApplyRaffleByGoodsId(@PathVariable Long goodsId, @RequestBody Long point) {
+
+        return ApiResponse.onSuccess(new Result(point, goodsId, LocalDateTime.now()));
+    }
+
+    @AllArgsConstructor
+    private static class Result {
+        @JsonProperty("point")
+        private Long point;
+
+        @JsonProperty("goods_id")
+        private Long goods_id;
+
+        @JsonProperty("updated_at")
+        private LocalDateTime updated_at;
+    }
 
     // 인증인가 구현되고 token 받게 되면 수정 예정
     @Operation(summary = "사원별 진행중인 응모 내역 조회", description = "사원이 응모한 응모 진행중인 상품들과 그 내역을 확인할 수 있습니다.")
