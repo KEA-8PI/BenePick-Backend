768e98adb9652782b197b6b3c1a815a58407525a
HyunseoPark <105943940+hspark-1@users.noreply.github.com>
2024-07-30 17:42:50 +0900

    Api 141 [FEAT] Draw&Raffle 도메인 목업코드 작성, 당첨자 상태관리 API구현 (#18)
    * Feat: mock-up code for draws & raffles
    
    * Merge: Integrate changes from develop branch
    
    * Feat: Apply Raffles By GoodsID
    
    * Feat: Added DrawsRequest DTO
    Feat: API-166 <당첨자 상태관리 API구현>
    
    * Fix: Delete result dto in Controller
    
    * Fix: getAllRafflesByGoodsId ADMIN Page
    
    * Fix: editWinnerStatus DRAW-004 ADMIN Page
    
    * Fix: downloadExcel DRAW-005 ADMIN Page
    
    * Fix: Comment Apply
    
    * Fix: Replace IOException to ApiException

--- src/main/java/com/_pi/benepick/domain/draws/controller/DrawsController.java
+++ src/main/java/com/_pi/benepick/domain/draws/controller/DrawsController.java
@@ -1,16 +1,31 @@
 package com._pi.benepick.domain.draws.controller;
 
+import com._pi.benepick.domain.draws.dto.DrawsRequest;
+import com._pi.benepick.domain.members.entity.Members;
+import com._pi.benepick.domain.members.repository.MembersRepository;
+import com._pi.benepick.global.common.exception.ApiException;
+import com._pi.benepick.global.common.response.code.status.ErrorStatus;
+import jakarta.servlet.http.HttpServletResponse;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import com._pi.benepick.domain.draws.dto.DrawsResponse;
+import com._pi.benepick.domain.draws.entity.Status;
 import com._pi.benepick.domain.draws.service.DrawsQueryService;
+import com._pi.benepick.domain.raffles.controller.RafflesController;
 import com._pi.benepick.global.common.response.ApiResponse;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,6 +35,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class DrawsController {
 
     private final DrawsQueryService drawsQueryService;
+    private final MembersRepository membersRepository;
 
     @Operation(summary = "상품별 당첨자 조회", description = "상품에 응모한 사원들 중 추첨 결과가 대기가 아닌 사원들과 그 내역을 확인할 수 있습니다.")
     @GetMapping("/winners/{goodsId}")
@@ -35,9 +51,23 @@ public class DrawsController {
 
     // 인증인가 구현되고 token 받게 되면 수정 예정
     @Operation(summary = "사원별 종료된 응모 내역 조회", description = "사원이 응모한 추첨 종료된 상품들과 그 내역을 확인할 수 있습니다.")
-    @GetMapping("/complete/raffles/{memberId}")
+    @GetMapping("/complete/list/{memberId}")
     public ApiResponse<DrawsResponse.DrawsResponseByMembersListDTO> getCompleteRafflesByMemberId(@PathVariable String memberId) {
         return ApiResponse.onSuccess(drawsQueryService.getCompleteRafflesByMemberId(memberId));
+    }
+
+    @Operation(summary = "당첨자 상태 관리 - Mockup API", description = "당첨자들의 상태를 관리할 수 있습니다.")
+    @PatchMapping("/winners/edit/{winnersId}")
+    public ApiResponse<DrawsResponse.DrawsResponseByMembersDTO> editWinnerStatus(@PathVariable Long winnersId, @RequestBody DrawsRequest.DrawsRequestDTO dto) {
+        Members member = membersRepository.findById("string").orElseThrow(() -> new ApiException(ErrorStatus._UNAUTHORIZED));
+        return ApiResponse.onSuccess(drawsQueryService.editWinnerStatus(member, winnersId, dto));
+    }
+
+    @Operation(summary = "추첨 결과 다운로드 - Mockup API", description = "추첨 결과가 정리된 엑셀 파일을 다운로드 할 수 있습니다.")
+    @GetMapping("/download/{goodsId}")
+    public void downloadExcel(@PathVariable Long goodsId, HttpServletResponse response) {
+        Members member = membersRepository.findById("string").orElseThrow(() -> new ApiException(ErrorStatus._UNAUTHORIZED));
+        drawsQueryService.downloadExcel(member, goodsId, response);
     }
 
 }
--- src/main/java/com/_pi/benepick/domain/raffles/controller/RafflesController.java
+++ src/main/java/com/_pi/benepick/domain/raffles/controller/RafflesController.java
@@ -1,9 +1,13 @@
 package com._pi.benepick.domain.raffles.controller;
 
+import com._pi.benepick.domain.members.entity.Members;
+import com._pi.benepick.domain.members.repository.MembersRepository;
+import com._pi.benepick.domain.raffles.dto.RafflesRequest;
 import com._pi.benepick.domain.raffles.dto.RafflesResponse;
 import com._pi.benepick.domain.raffles.service.RafflesQueryService;
-import com._pi.benepick.domain.raffles.service.RafflesQueryServiceImpl;
+import com._pi.benepick.global.common.exception.ApiException;
 import com._pi.benepick.global.common.response.ApiResponse;
+import com._pi.benepick.global.common.response.code.status.ErrorStatus;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
@@ -11,8 +15,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,6 +24,14 @@ import java.util.List;
 public class RafflesController {
 
     private final RafflesQueryService rafflesQueryService;
+    private final MembersRepository membersRepository;
+
+    // 인증인가 구현되고 token 받게 되면 수정 예정
+    @Operation(summary = "응모하기", description = "물품 아이디, 포인트를 사용하여 응모합니다.")
+    @PostMapping("/apply/{goodsId}/{memberId}")
+    public ApiResponse<RafflesResponse.RafflesResponseByGoodsDTO> getApplyRaffleByGoodsId(@PathVariable String memberId, @PathVariable Long goodsId, @RequestBody RafflesRequest.RafflesRequestDTO raffleAddDTO) {
+        return ApiResponse.onSuccess(rafflesQueryService.applyRaffle(memberId, goodsId, raffleAddDTO));
+    }
 
     // 인증인가 구현되고 token 받게 되면 수정 예정
     @Operation(summary = "사원별 진행중인 응모 내역 조회", description = "사원이 응모한 응모 진행중인 상품들과 그 내역을 확인할 수 있습니다.")
@@ -33,7 +43,7 @@ public class RafflesController {
     @Operation(summary = "상품별 응모자 조회", description = "상품에 응모한 사원들과 그 내역을 확인할 수 있습니다.")
     @GetMapping("/applicant/{goodsId}")
     public ApiResponse<RafflesResponse.RafflesResponseByGoodsListDTO> getAllRafflesByGoodsId(@PathVariable Long goodsId) {
-        return ApiResponse.onSuccess(rafflesQueryService.getAllRafflesByGoodsId(goodsId));
+        Members member = membersRepository.findById("string").orElseThrow(() -> new ApiException(ErrorStatus._UNAUTHORIZED));
+        return ApiResponse.onSuccess(rafflesQueryService.getAllRafflesByGoodsId(member, goodsId));
     }
-
 }
