d58867313f9736d957244f48a89f39d11bd8dfdf
2sojeong <dlgur200123@naver.com>
2024-07-30 14:54:05 +0900

    Feat:사원 목록 조회 및 검색

--- src/main/java/com/_pi/benepick/domain/members/service/MembersQueryService.java
+++ src/main/java/com/_pi/benepick/domain/members/service/MembersQueryService.java
@@ -1,4 +1,7 @@
 package com._pi.benepick.domain.members.service;
 
+import com._pi.benepick.domain.members.dto.MembersResponse;
+
 public interface MembersQueryService {
+    MembersResponse.MembersDetailListResponseDTO getMembersList(Integer page, Integer size, String keyword);
 }
