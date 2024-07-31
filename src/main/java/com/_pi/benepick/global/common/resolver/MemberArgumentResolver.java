a9a14ee7bff069f87cced7ac2605846882f424a3
SungHoon Jung <32007781+wooing1084@users.noreply.github.com>
2024-07-31 14:32:25 +0900

    feat: resolver 구현 (#28)

---
+++ src/main/java/com/_pi/benepick/global/common/resolver/MemberArgumentResolver.java
@@ -0,0 +1,40 @@
+package com._pi.benepick.global.common.resolver;
+
+import com._pi.benepick.domain.members.entity.Members;
+import com._pi.benepick.domain.members.repository.MembersRepository;
+import com._pi.benepick.global.common.annotation.MemberObject;
+import com._pi.benepick.global.common.jwt.JwtTokenProvider;
+import jakarta.servlet.http.HttpServletRequest;
+import java.util.Objects;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+@Component
+@RequiredArgsConstructor
+public class MemberArgumentResolver implements HandlerMethodArgumentResolver {
+    private final JwtTokenProvider jwtTokenProvider;
+    private final MembersRepository membersRepository;
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.getParameterAnnotation(MemberObject.class) != null &&
+            parameter.getParameterType().equals(Members.class);
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
+        NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        String token = jwtTokenProvider.resolveToken(
+            Objects.requireNonNull(webRequest.getNativeRequest(HttpServletRequest.class)));
+
+        if(token == null)
+            return null;
+
+        return membersRepository.findById(jwtTokenProvider.getUserPk(token)).orElse(null);
+    }
+}
