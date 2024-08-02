c395289d1c7007df57c2aad059a595d9e6ebcd0f
Somi Nam <somi4219@gmail.com>
2024-08-02 13:54:11 +0900

    Api-169-[feat] 상품 엑셀 파일 이미지 파싱 구현 (#31)
    * fix : 이전 PR merge 후 바뀐 코드 재수정
    
    * feat : 파일 파싱
    
    * feat : 오브젝트 스토리지 추가
    
    * feat : 사진 추출 후 오브젝트 스토리지 저장 구현
    
    * feat : 사진 추출 후 오브젝트 스토리지 저장 구현
    
    * fix : 오류 수정
    
    * refactor : 이미지 업로드 controller 디렉토리 수정
    
    * feat : 이미지 업로드 제외
    
    * fix : response 수정
    
    * fix : 오류수정
    
    * fix : response에 id 추가
    
    * refactor: 이미지 관련 주석 삭제
    
    * refactor: objectstorage 리팩토링
    
    ---------
    
    Co-authored-by: wooing1084 <kmjung1515@naver.com>

--- src/main/java/com/_pi/benepick/domain/goods/dto/GoodsResponse.java
+++ src/main/java/com/_pi/benepick/domain/goods/dto/GoodsResponse.java
@@ -133,26 +133,7 @@ public class GoodsResponse {
         private List<GoodsSearchResponseDTO> goodsSearchDTOList;
     }
 
-    //상품 파일 업로드
     // 상품 추가 및 수정
-    @Builder
-    @Getter
-    @AllArgsConstructor(access = AccessLevel.PRIVATE)
-    @NoArgsConstructor
-    public static class GoodsUploadResponseDTO {
-        private String result;
-        public static GoodsUploadResponseDTO createSuccessResponse() {
-            return GoodsUploadResponseDTO.builder()
-                    .result("추가되었습니다.")
-                    .build();
-        }
-        public static GoodsUploadResponseDTO createFailureResponse() {
-            return GoodsUploadResponseDTO.builder()
-                    .result("실패하였습니다.")
-                    .build();
-        }
-    }
-
     @Builder
     @Getter
     @AllArgsConstructor(access = AccessLevel.PRIVATE)
@@ -185,6 +166,14 @@ public class GoodsResponse {
                     .category(category)
                     .build();
         }
+    }
+    // 상품 파일 업로드
+    @Builder
+    @Getter
+    @AllArgsConstructor(access = AccessLevel.PRIVATE)
+    @NoArgsConstructor
+    public static class GoodsUploadResponseDTO {
+        private List<GoodsAddResponseDTO> goodsUploadDTOList;
     }
 
     // 상품 삭제
