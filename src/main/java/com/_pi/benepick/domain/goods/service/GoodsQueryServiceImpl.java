c1ea18644d89dce7c7201a32c7a2135c706dd09e
Somi Nam <somi4219@gmail.com>
2024-08-02 13:59:42 +0900

    fix : readOnly 처리

--- src/main/java/com/_pi/benepick/domain/goods/service/GoodsQueryServiceImpl.java
+++ src/main/java/com/_pi/benepick/domain/goods/service/GoodsQueryServiceImpl.java
@@ -24,7 +24,7 @@ import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
-@Transactional
+@Transactional(readOnly = true)
 public class GoodsQueryServiceImpl implements GoodsQueryService {
 
     private final GoodsRepository goodsRepository;
