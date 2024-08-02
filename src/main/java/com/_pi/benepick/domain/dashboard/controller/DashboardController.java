f9cf00ab17829e5da0cac882f49f0ee8e1e0c483
Somi Nam <somi4219@gmail.com>
2024-08-02 14:25:31 +0900

    feat : controller 변경

--- src/main/java/com/_pi/benepick/domain/dashboard/controller/DashboardController.java
+++ src/main/java/com/_pi/benepick/domain/dashboard/controller/DashboardController.java
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,7 +22,7 @@ import java.util.List;
 public class DashboardController {
     @Operation(summary = "대시보드 조회 - Mockup API", description = "대시보드를 조회합니다.")
     @GetMapping("/dashboard")
-    public ApiResponse<DashboardResponse.DashboardResponseDTO> getGoodsList(@RequestParam String category, @RequestParam LocalDateTime startDate, @RequestParam LocalDateTime endDate) {
+    public ApiResponse<DashboardResponse.DashboardResponseDTO> getDashboard(@RequestParam String category, @RequestParam @DateTimeFormat LocalDateTime startDate, @RequestParam @DateTimeFormat LocalDateTime endDate) {
         Double result = 1.0;
         Double result1 = 2.0;
         List<Double> resultList = Arrays.asList(result, result1);
