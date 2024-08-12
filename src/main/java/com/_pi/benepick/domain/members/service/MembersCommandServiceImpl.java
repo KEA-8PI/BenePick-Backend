8ee593ab37c1ead86f3e93dbbbcdeb0f671ac0b3
2sojeong <dlgur200123@naver.com>
2024-08-12 17:56:16 +0900

    Feat:사원 추가시 포인트, 패널티 초기값 히스토리 추가

--- src/main/java/com/_pi/benepick/domain/members/service/MembersCommandServiceImpl.java
+++ src/main/java/com/_pi/benepick/domain/members/service/MembersCommandServiceImpl.java
@@ -1,32 +1,18 @@
 package com._pi.benepick.domain.members.service;
 import com._pi.benepick.domain.members.dto.MembersRequest.*;
-import com._pi.benepick.domain.members.dto.MembersResponse;
 import com._pi.benepick.domain.members.dto.MembersResponse.*;
 import com._pi.benepick.domain.members.entity.Members;
-import com._pi.benepick.domain.members.repository.MembersRepository;
 import com._pi.benepick.global.common.exception.ApiException;
 import com._pi.benepick.global.common.response.code.status.ErrorStatus;
-import com._pi.benepick.domain.members.entity.Role;
-import java.util.Optional;
 import lombok.RequiredArgsConstructor;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.xssf.usermodel.XSSFSheet;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.multipart.MultipartFile;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.List;;
 
 @Service
 @RequiredArgsConstructor
 @Transactional
 public class MembersCommandServiceImpl implements MembersCommandService{
 
-    private final MembersRepository membersRepository;
 
     @Override
     public MembersuccessDTO changePassword(MemberPasswordDTO memberPasswordDTO, Members members){
@@ -50,18 +36,5 @@ public class MembersCommandServiceImpl implements MembersCommandService{
         return password.matches(passwordRegex);
     }
 
-    public MembersDetailResponseDTO addMembers(AdminMemberRequestDTO membersRequestDTO,Members member){
-        if(membersRepository.findById(membersRequestDTO.getId()).isPresent()){
-            throw new ApiException(ErrorStatus._ALREADY_EXIST_MEMBER);
-        }
 
-        if(member.getRole() == Role.MEMBER){
-            throw new ApiException(ErrorStatus._UNAUTHORIZED);
-        }
-
-        Members members=membersRequestDTO.toEntity(membersRequestDTO);
-        membersRepository.save(members);
-
-        return MembersDetailResponseDTO.from(members);
-    }
 }
