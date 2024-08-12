568e20655043b5720bcf75b71626ec33fcdce55d
SungHoon Jung <32007781+wooing1084@users.noreply.github.com>
2024-08-12 15:09:17 +0900

    Api 325 [REFACTOR] Common과 인증 도메인 리팩토링 (#78)
    * refactor: auth 코드 리팩토링
    
    * refactor: common부분 refactor

--- src/main/java/com/_pi/benepick/global/common/jwt/service/JwtQueryService.java
+++ src/main/java/com/_pi/benepick/global/common/jwt/service/JwtQueryService.java
@@ -3,7 +3,6 @@ package com._pi.benepick.global.common.jwt.service;
 import com._pi.benepick.global.common.jwt.dto.JwtResponse.JwtPairDTO;
 
 public interface JwtQueryService {
-
     JwtPairDTO findJwtPair(String accessToken);
 
 }
