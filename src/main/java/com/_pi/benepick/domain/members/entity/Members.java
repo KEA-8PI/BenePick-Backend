a9208bf0366958e26826801717a013263fb62af0
2sojeong <dlgur200123@naver.com>
2024-08-12 21:04:37 +0900

    Feat: 초기 비밀번호 hash 적용

--- src/main/java/com/_pi/benepick/domain/members/entity/Members.java
+++ src/main/java/com/_pi/benepick/domain/members/entity/Members.java
@@ -74,7 +74,8 @@ public class Members extends BaseJPATimeEntity {
     }
 
     public void initPassword( PasswordEncoder passwordEncoder){
-        this.password=passwordEncoder.encode("0000");
+        // detault 비밀번호 0000 hash 값
+        this.password=passwordEncoder.encode("9af15b336e6a9619928537df30b2e6a2376569fcf9d7e773eccede65606529a0");
     }
 
 }
