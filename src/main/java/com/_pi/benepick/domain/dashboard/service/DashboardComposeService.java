47677bcbfd750e31315843b629fbc127767799d7
Hyunseok Lee <110045522+Hyunstone@users.noreply.github.com>
2024-08-15 21:08:51 +0900

    Api 373 refactor/dashboard api (#105)
    * refactor: 대시보드 api 분리
    
    합쳐져있던 대시보드 api -> 5개의 api로 분리
    
    * fix: localdatetime -> localdate
    
    * feat: dashboard api cache 적용
    
    ---------
    
    Co-authored-by: wooing1084 <kmjung1515@naver.com>

--- src/main/java/com/_pi/benepick/domain/dashboard/service/DashboardComposeService.java
+++ src/main/java/com/_pi/benepick/domain/dashboard/service/DashboardComposeService.java
@@ -2,8 +2,19 @@ package com._pi.benepick.domain.dashboard.service;
 
 import com._pi.benepick.domain.dashboard.dto.DashboardResponse;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 public interface DashboardComposeService {
     DashboardResponse.DashboardResponseDTO getDashboard(String category, LocalDateTime startDate, LocalDateTime endDate);
+
+    DashboardResponse.WinnerPointsPerRafflesDto getWinnerPointsPerRaffles(String category, LocalDate startDate, LocalDate endDate);
+
+    DashboardResponse.TotalPointsPerRafflesDto getTotalPointsPerRaffles(String category, LocalDate startDate, LocalDate endDate);
+
+    DashboardResponse.RefillRatesPerRafflesDto getRefillRatesPerRaffles(String category, LocalDate startDate, LocalDate endDate);
+
+    DashboardResponse.MostWonRanksDto getMostWonRanks(String category, LocalDate startDate, LocalDate endDate);
+
+    DashboardResponse.AvgWinnerPointsDto getAvgWinnerPoints(String category, LocalDate startDate, LocalDate endDate);
 }
