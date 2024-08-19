ddf6b1c15140d3114cc07fddda89653c3545ab7b
BSY <45937750+ideawolf@users.noreply.github.com>
2024-08-19 17:04:59 +0900

    chore: Swagger Config  배포시 http로 요청되는거 해결

--- src/main/java/com/_pi/benepick/config/SwaggerConfig.java
+++ src/main/java/com/_pi/benepick/config/SwaggerConfig.java
@@ -5,6 +5,8 @@ import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -14,7 +16,8 @@ import org.springframework.context.annotation.Configuration;
         version = "v0.0.0",
         description = "Documentation Benepick Service API v0.0.0"
     ),
-    security = @SecurityRequirement(name = "bearerAuth")
+    security = @SecurityRequirement(name = "bearerAuth"),
+    servers = {@Server(url = "/", description = "Default Server URL")}
 )
 @SecurityScheme(
     name = "bearerAuth",
